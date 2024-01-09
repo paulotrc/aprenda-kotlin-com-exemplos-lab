aed123a271540126f2619d6879613976cf9e8b47
Paulo Carneiro <paulotrc@gmail.com>
2024-01-09 20:07:00 -0300

    [UPDATE] Teste

--- src/main/kotlin/paulotrc/Desafio.kt
+++ src/main/kotlin/paulotrc/Desafio.kt
@@ -6,6 +6,7 @@ import main.kotlin.paulotrc.dataclasses.Usuario
 fun main() {
 //    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
 //    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //Teste
 
     val desafio = Desafio()
     val usuario = Usuario(nome="Paulo", matricula="1")
